3a9d643091004d68e9389ce3c8c76859acf014ac
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-03-26 18:31:37 +0100

    Update input.c

--- source/input.c
+++ source/input.c
@@ -1,21 +1,36 @@
 // source/input.c
 #include "input.h"
 #include <vpad/input.h>
+#include <kpad/kpad.h>
 
 static VPADStatus vpad_data;
 static uint32_t prev_buttons = 0;
+static uint32_t current_buttons = 0;
 
 void input_init() {
     VPADInit();
+    KPADInit();
 }
 
 void input_scan() {
+    prev_buttons = current_buttons;
+    current_buttons = 0;
+
+    // --- GamePad ---
     VPADRead(0, &vpad_data, 0, NULL);
+    current_buttons |= vpad_data.hold;
+
+    // --- Wiimotes / Pro / Classic Controllers ---
+    for (int i = 0; i < 4; i++) {
+        KPADStatus kpad_data;
+        int count = 0;
+        KPADRead(i, &kpad_data, 1, &count);
+        if (count > 0) {
+            current_buttons |= kpad_data.hold;
+        }
+    }
 }
 
 bool input_pressed(uint32_t button) {
-    uint32_t curr = vpad_data.hold;
-    uint32_t down = curr & ~prev_buttons;
-    prev_buttons = curr;
-    return (down & button) != 0;
+    return (current_buttons & button) && !(prev_buttons & button);
 }
