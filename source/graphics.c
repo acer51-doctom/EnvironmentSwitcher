8c440c28872219702c4f536239a8856752b930e2
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-03-26 12:45:38 +0100

    Update graphics.c

--- source/graphics.c
+++ source/graphics.c
@@ -1,33 +1,30 @@
 // source/graphics.c
 #include "graphics.h"
+#include "environment.h"
+#include <coreinit/screen.h>
 #include <stdio.h>
 #include <string.h>
-#include <gx2/screen.h>
-#include <coreinit/screen.h>
-#include <vpad/input.h>
 
 #define MAX_ROW_LENGTH 100
 #define BASE_Y 40
 #define LINE_HEIGHT 24
-
-static OSScreenID screen_buf = SCREEN_TV;
+#define SCREEN_ID SCREEN_TV
 
 void gfx_init() {
-    screen_buf = SCREEN_TV;
     OSScreenInit();
     OSScreenSetBufferEx(SCREEN_TV, (void *)0xF4000000);
     OSScreenEnableEx(SCREEN_TV, true);
 }
 
 void gfx_clear() {
-    OSScreenClearBufferEx(screen_buf, 0); // black screen
+    OSScreenClearBufferEx(SCREEN_ID, 0);  // 0 = black
 }
 
 void gfx_draw_text(int row, const char *text) {
     if (!text) return;
 
     int y = BASE_Y + row * LINE_HEIGHT;
-    OSScreenPutFontEx(screen_buf, 0, y, text);
+    OSScreenPutFontEx(SCREEN_ID, 0, y, text);
 }
 
 void gfx_draw_welcome() {
@@ -40,7 +37,6 @@ void gfx_draw_welcome() {
 
 void gfx_draw_env_list(Environment *envs, int env_count, int selected_index) {
     gfx_clear();
-
     gfx_draw_text(0, "Available Environments:");
 
     for (int i = 0; i < env_count; ++i) {
@@ -52,12 +48,12 @@ void gfx_draw_env_list(Environment *envs, int env_count, int selected_index) {
                  envs[i].name,
                  descriptor);
 
-        gfx_draw_text(i + 2, line); // offset to skip title
+        gfx_draw_text(i + 2, line); // +2 to leave space at top
     }
 
     gfx_present();
 }
 
 void gfx_present() {
-    OSScreenFlipBuffersEx(screen_buf);
+    OSScreenFlipBuffersEx(SCREEN_ID);
 }
